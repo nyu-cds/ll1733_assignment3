6b8bc5e1830b9ca3e89947864efebd38659701b5
ll1733 <lingboli@Lingbos-MacBook-Pro.local>
2017-04-30 22:15:13 -0400

    Assignment12 part1 revised

--- mandelbrot_gpu.py
+++ mandelbrot_gpu.py
@@ -27,40 +27,30 @@ def compute_mandel(min_x, max_x, min_y, max_y, image, iters):
     The code first obtains the starting x and y coordinates using
     cuda.grid() function.
     
-    The it obtains the total units of x and y, namely
-    the production of gridDim and blockDim.
-    
-    Next use image's width and height to divide the total units of
-    x and that of y correspondingly, and the results are xRange and
-    yRange, which represent the number of steps to go from starting x and
-    y coordinates to finishing x and y coordinates.
-    
-    To calculate the values for real and imag, the old "x" and "y" are
-    substituted by xoffset and yoffset, which are adjusted based on starting
-    indices and unit steps.
-    """
-    xstart, ystart = cuda.grid(2)  ## starting indices of x and y
+    Then it obtains the ending values of x and y, namely the edges for
+    the image (image.shape[0], image.shape[1]).
 
-    xcapacity=cuda.gridDim.x*cuda.blockDim.x      ##  total units of x
-    ycapacity=cuda.gridDim.y*cuda.blockDim.y      ##  total units of y
+    Next it finds the step values for x and y,namely the production of gridDim and blockDim.
+
+    Finally, it uses the derived "start", "end", and "step" values to construct the for
+    loop to start iteration.
+    """
+    xstart, ystart = cuda.grid(2)  ## starting values of x and y
+
+
+    height,width=image.shape[0],image.shape[1]     ## boundaried of the image, namely ending values for y and x
+   
+    xstep=cuda.gridDim.x*cuda.blockDim.x  ## the shape of the grid in x-axis designates the step for x
+    ystep=cuda.gridDim.y*cuda.blockDim.y  ## the shape of the grid in y-axis designates the step for y 
     
-    xRange=int((image.shape[1])/(xcapacity)) ##getting evenly mapping between image and grid for x
-    yRange=int((image.shape[0])/(ycapacity))  ##getting evenly mapping between image and grid for y
-    
-    pixel_size_x = (max_x - min_x) / image.shape[1]
-    pixel_size_y = (max_y - min_y) / image.shape[0]
-    for x in range(xRange):
-        xoffset=xstart+xcapacity*x
-        if xoffset>=image.shape[1]:
-            break
-        real = min_x + xoffset * pixel_size_x
-        for y in range(yRange):
-            yoffset=ystart+ycapacity*y
-            
-            if yoffset >=image.shape[0]:
-                break
-            imag = min_y + yoffset * pixel_size_y
-            image[yoffset, xoffset] = mandel(real, imag, iters)
+   
+    pixel_size_x = (max_x - min_x) / width
+    pixel_size_y = (max_y - min_y) / height
+    for x in range(xstart,width,xstep):  ## start, end, step
+        real = min_x + x * pixel_size_x
+        for y in range(ystart,height,ystep): ## start, end , step
+            imag = min_y + y * pixel_size_y
+            image[y, x] = mandel(real, imag, iters)
         
 if __name__ == '__main__':
     image = np.zeros((1024, 1536), dtype = np.uint8)
